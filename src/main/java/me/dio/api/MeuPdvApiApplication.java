3c8a2d24b4f662093e425668eebd3cee68e27396
Matheuscunha20 <matheu20pereira15@gmail.com>
2025-03-14 09:24:50 -0300

    Create MeuPdvApiApplication.java

---
+++ src/main/java/me/dio/api/MeuPdvApiApplication.java
@@ -0,0 +1,12 @@
+package me.dio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPdvApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuPdvApiApplication.class, args);
+    }
+}
