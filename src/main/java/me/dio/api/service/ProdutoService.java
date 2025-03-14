4907f9e6aa1a69bfe4ba642ccf06c9d56c6ca997
Matheuscunha20 <matheu20pereira15@gmail.com>
2025-03-14 09:27:13 -0300

    Create ProdutoService.java

---
+++ src/main/java/me/dio/api/service/ProdutoService.java
@@ -0,0 +1,23 @@
+package me.dio.api.service;
+
+import me.dio.api.model.Produto;
+import me.dio.api.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto saveProduto(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public List<Produto> getAllProdutos() {
+        return produtoRepository.findAll();
+    }
+}
