fe8cf23aad9c980516d399f1d9ef31da1157c351
Matheuscunha20 <matheu20pereira15@gmail.com>
2025-03-14 09:26:22 -0300

    Create Produto.java

---
+++ src/main/java/me/dio/api/model/Produto.java
@@ -0,0 +1,18 @@
+package me.dio.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Double preco;
+
+    // Getters e Setters
+}
