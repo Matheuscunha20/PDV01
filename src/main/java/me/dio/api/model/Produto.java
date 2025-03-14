0f69485fe40bd558417d26e4a5a22bf82975b84c
Matheuscunha20 <matheu20pereira15@gmail.com>
2025-03-14 09:39:15 -0300

    Update Produto.java

--- src/main/java/me/dio/api/model/Produto.java
+++ src/main/java/me/dio/api/model/Produto.java
@@ -15,4 +15,28 @@ public class Produto {
     private Double preco;
 
     // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
 }
